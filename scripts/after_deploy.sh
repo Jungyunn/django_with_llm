b54d854d5bc2e708071a6204cf39b89747491999
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-12-03 16:16:22 +0900

    Upload after_deploy

---
+++ scripts/after_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+REPOSITORY=/home/ubuntu/
+
+cd $REPOSITORY/django_with_llm
+
+echo "> 🔵 Stop & Remove docker services."
+sudo docker-compose down
+
+echo "현재 디렉토리: $(pwd)"
+
+echo "> 🟢 Run new docker services."
+sudo docker-compose up -d --build >> /var/log/deploy.log 2>&1
+
+echo "스크립트 종료"
